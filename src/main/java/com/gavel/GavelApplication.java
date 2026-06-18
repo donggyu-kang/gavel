5e7d3b644b07ebabfefb272b4536c27ef2df0dd1
강동규 <kangkg0067@gmail.com>
2026-06-18 16:03:58 +0900

    chore: Spring Initializer로 프로젝트 초기 설정

---
+++ src/main/java/com/gavel/GavelApplication.java
@@ -0,0 +1,13 @@
+package com.gavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GavelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gavel/GavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
